09c551257ab6bf85ab8bc4edd0f42323db8c2f94
Valithor Obsidion <valzargaming@gmail.com>
2021-04-24 19:54:12 -0400

    Create Helix.php

---
+++ Twitch/Helix.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is a part of the TwitchPHP project.
+ *
+ * Copyright (c) 2021 ValZarGaming <valzargaming@gmail.com>
+ */
+
+namespace Twitch;
+
+/**
+ * Provides an easy way to have triggerable commands.
+ */
+class HelixCommandClient extends Twitch
+{
+	
+}
+?>
